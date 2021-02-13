5943553eb42a12434218d97c0df70f24b48cfd2d
Sebas-Armas <armas.sebas@gmail.com>
2021-02-12 20:05:09 -0500

    IMpl

---
+++ Assets/Scripts/Objects.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+ public class Objects
+ {
+     public string name;
+     public string house;
+     public string ancestry; //ascendencia
+     public string patronus;
+
+    public override string ToString()
+    {
+        return "Name: " + name + "\nHouse: "+ house + "\nAncestry: " + ancestry + "\nPatronus: " + patronus;
+    }
+}
+ 
+ [Serializable]
+ public class RootObject
+ {
+     public Objects[] characters;
+
+ }
---
+++ Assets/Scripts/readJson.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class readJson : MonoBehaviour
+{
+    public string jsonUrl;
+    public  TextMeshProUGUI information;
+
+    public void getCharacter(int index)
+    {
+        StartCoroutine(getData(index));
+    }
+
+    IEnumerator getData(int index)
+    {
+        WWW _www = new WWW(jsonUrl);
+        yield return _www;
+
+        if(_www.error == null)
+        {
+            RootObject myObject = JsonUtility.FromJson<RootObject>("{\"characters\":" + _www.text + "}");
+
+            information.text = myObject.characters[index].ToString();
+            Debug.Log(myObject.characters[index]);
+
+        }
+        else
+        {
+            Debug.Log("Error");
+        }
+    }
+}
+
